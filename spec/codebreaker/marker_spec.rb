ec03c786c89af21374198fbb995fdf708e6f61dc
Twin Panichsombat <roofimon@gmail.com>
2012-12-28 15:35:23 +0700

    Finish :)

--- spec/codebreaker/marker_spec.rb
+++ spec/codebreaker/marker_spec.rb
@@ -31,27 +31,31 @@ module Codebreaker
     end
 
     describe "#number_match_count" do
+
+      context "with one exact match duplicated in guess" do
+        it "returns 0" do
+          marker = Marker.new('1234', '1155')
+          marker.number_match_count.should == 0
+        end
+      end
       context "with no matches" do
         it "returns 0" do
           marker = Marker.new('1234','5555')
           marker.number_match_count.should == 0
         end
       end
-
       context "with 1 number match" do
         it "returns 1" do
           marker = Marker.new('1234','2555')
           marker.number_match_count.should == 1
         end
       end
-
       context "with 1 exact match" do
         it "returns 0" do
           marker = Marker.new('1234','1555')
           marker.number_match_count.should == 0
         end
       end
-
       context "with 1 exact match and 1 number match" do
         it "returns 1" do
           marker = Marker.new('1234','1525')
